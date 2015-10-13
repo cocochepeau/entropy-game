39fba768e87e52cdd1251d0dcdc8d43260c739c1
cocochepeau <cocochepeau@gmail.com>
2015-10-13 09:12:29 +0200

    game skeleton

---
+++ libs/Game.class.php
@@ -0,0 +1,17 @@
+<?php
+class Game
+{
+	private $playerOne;
+	private $playerTwo;
+	private $board;
+	
+	public function __construct($playerOne, $playerTwo)
+	{
+		
+	}
+
+	public function endGame()
+	{
+		
+	}
+}
