d2bd07e11d2a68eb310a5526d8cd18edf0939d54
cocochepeau <cocochepeau@gmail.com>
2015-10-13 11:16:31 +0200

    on our way to namespaces

--- libs/autoloader.php
+++ libs/autoloader.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * An example of a project-specific implementation.
+ * 
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
+ * from /path/to/project/src/Baz/Qux.php:
+ * 
+ *      new \Foo\Bar\Baz\Qux;
+ *      
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
 spl_autoload_register(function ($class)
 {
 	// project-specific namespace prefix
