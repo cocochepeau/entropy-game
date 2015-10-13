38ea2f6d3249575fa74386a1a1131b4bfdc93d95
cocochepeau <cocochepeau@gmail.com>
2015-10-13 08:29:16 +0200

    index page skeleton

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Entropy Game</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+	</head>
+	<body>
+		
+		<script async src="assets/js/jquery-1.11.3.min.js"></script>
+		<script async src="assets/js/custom.js"></script>
+	</body>
+</html>
