7a18be85235433c12f909236178c3b1114f883dd
cocochepeau <cocochepeau@gmail.com>
2015-10-13 08:40:27 +0200

    index edited

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+if(isset($_POST['']))
+{
+	$playerOne = new Player();
+	$playerTwo = new Player();
+	$game = new Game();
+}
 
 
 ?>
@@ -10,7 +16,30 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
 	</head>
 	<body>
-		
+		<?php
+		if($game)
+		{
+			?>
+			<div class="board">
+				<?php $game->drawBoard(); ?>
+			</div>
+			<div class="mgr">
+				<form action="index.php" method="post">
+					<button type="submit" name="restart">Relancer</button>
+				</form>
+			</div>
+			<?php
+		}
+		else
+		{
+			?>
+			<h1>Entropy Game</h1>
+			<form action="index.php" method="post" accept-charset="UTF-8">
+				
+			</form>
+			<?php
+		}
+		?>
 		<script async src="assets/js/jquery-1.11.3.min.js"></script>
 		<script async src="assets/js/custom.js"></script>
 	</body>
