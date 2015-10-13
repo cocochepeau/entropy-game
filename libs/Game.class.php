ebf36036aba744592dbca1fe39acc91308000349
Selim DAHMANI <E145322J@U-IUT-J-C004-12.iut-nantes.univ-nantes.prive>
2015-10-13 14:07:38 +0200

    Change parameter __contruct function

--- libs/Game.class.php
+++ libs/Game.class.php
@@ -5,13 +5,20 @@ class Game
 	private $playerTwo;
 	private $board;
 	
-	public function __construct($playerOne, $playerTwo)
-	{
-		
+	public function __construct($playerNameOne, $playerNameTwo) {
+		$playerOne = new Player($playerNameOne);
+		$playerTwo = new Player($playerNameTwo);
+
+	//initialisation of board	
+		$board =array();
+		$board[1] = array(new Pawn($playerOne), new Pawn($playerOne), new Pawn($playerOne), new Pawn($playerOne), new Pawn($playerOne));
+		$board[2] = array(new Pawn($playerOne), null, null, null, new Pawn($playerOne));
+		$board[3] = array(null, null, null, null, null);
+		$board[4] = array(new Pawn($playerTwo), null, null, null, new Pawn($playerTwo));
+		$board[5] = array(new Pawn($playerOne), new Pawn($playerTwo), new Pawn($playerTwo), new Pawn($playerTwo), new Pawn($playerTwo));
 	}
 
-	public function endGame()
-	{
+	public function endGame() {
 		if(
 			$this->playerOne->cantMove()
 			|| $this->playerTwo->cantMove()
